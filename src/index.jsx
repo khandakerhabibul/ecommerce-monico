ae0828e0da93f06f63f532e05e0c17b84df484fc
Khandaker Amin <khandakerhabibul@gmail.com>
2022-06-11 14:21:16 +0600

    feat: added redux-toolkit and completed setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App'
+import store from './store/store'
 import CustomThemeProvider from './Theme/CustomThemeProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <CustomThemeProvider>
-      <App />
-    </CustomThemeProvider>
+    <Provider store={store}>
+      <CustomThemeProvider>
+        <App />
+      </CustomThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
